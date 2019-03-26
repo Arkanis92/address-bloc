ad33b1893de80276963f04e1dd7456ad97679d51
Victor Kung <vkung15@gmail.com>
2019-03-26 12:09:25 -0400

    Testing Checkpoint

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,6 +9,7 @@ module.exports = class MenuController {
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
+          "Date",
           "Exit"
         ]
       }
@@ -22,6 +23,9 @@ module.exports = class MenuController {
       switch(response.mainMenuChoice) {
         case "Add new contact":
           this.addContact();
+          break;
+        case "Date":
+          this.getDate();
           break;
         case "Exit":
           this.exit();
@@ -48,5 +52,15 @@ module.exports = class MenuController {
   exit () {
     console.log("Thanks for using AddressBloc.");
     process.exit();
+  }
+
+  getContactCount () {
+    return this.contacts.length;
+  }
+
+  getDate () {
+    let ts = new Date();
+    console.log(ts.toString());
+    this.main();
   }
 }
