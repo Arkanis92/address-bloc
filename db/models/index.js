e17473195fdb734690e6d93d3e3af9157415149e
Victor Kung <vkung15@gmail.com>
2019-03-27 16:46:28 -0400

    Contacts and Connections Checkpoint 2

--- db/models/index.js
+++ db/models/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const configjson = require(__dirname + '/../config/config.json');
+const config = configjson[env];
 const db = {};
 
 let sequelize;
