aadf1953c897c9eb71cdfae8987101c55f4dfcd2
unknown <vkung15@gmail.com>
2019-04-02 16:55:41 -0400

    Listing Contacts and Searching Checkpoint 1

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -11,6 +11,7 @@ module.exports = class MenuController {
         choices: [
           "Add new contact",
           "View all contacts",
+          "Search for a contact",
           "Date",
           "Exit"
         ]
@@ -28,6 +29,9 @@ module.exports = class MenuController {
           break;
         case "View all contacts":
           this.getContacts();
+          break;
+        case "Search for a contact":
+          this.search();
           break;
         case "Date":
           this.getDate();
@@ -92,5 +96,74 @@ module.exports = class MenuController {
     let ts = new Date();
     console.log(ts.toString());
     this.main();
+  }
+
+  search() {
+    inquirer.prompt(this.book.searchQuestions)
+    .then((target) => {
+      this.book.search(target.name)
+      .then((contact) => {
+        if(contact === null) {
+          this.clear();
+          console.log("contact not found");
+          this.search();
+        } else {
+          this.showContact(contact);
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      this.main();
+    });
+  }
+
+  showContact(contact) {
+    this._printContact(contact);
+    inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected) {
+        case "Delete Contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+  }
+
+  _printContact(contact) {
+    console.log(`
+      name: ${contact.name}
+      phone number: ${contact.phone}
+      email: ${contact.email}
+      -------------`
+    );
+  }
+
+  delete(contact) {
+    inquirer.prompt(this.book.deleteConfirmQuestions)
+    .then((answer) => {
+      if(answer.confirmation) {
+        this.book.delete(contact.id);
+        console.log("contact deleted.");
+        this.main();
+      } else {
+        console.log("contact has not been deleted");
+        this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.main();
+    });
   }
 }
