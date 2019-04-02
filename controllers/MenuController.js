9dd4e6cdad38d47b0ff4368c9b7b875b81439c63
unknown <vkung15@gmail.com>
2019-04-02 13:45:13 -0400

    Listing Contacts and Searching Checkpoint Partial 1

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,6 +10,7 @@ module.exports = class MenuController {
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
+          "View all contacts",
           "Date",
           "Exit"
         ]
@@ -24,6 +25,9 @@ module.exports = class MenuController {
       switch(response.mainMenuChoice) {
         case "Add new contact":
           this.addContact();
+          break;
+        case "View all contacts":
+          this.getContacts();
           break;
         case "Date":
           this.getDate();
@@ -47,13 +51,31 @@ module.exports = class MenuController {
   addContact () {
     this.clear();
     inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-      this.book.addContact(answers.name, answer.phone).then((contact) => {
+      this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
         console.log("Contact added successfully");
         this.main();
       }).catch((err) => {
         console.log(err);
         this.main();
       });
+    });
+  }
+
+  getContacts () {
+    this.clear();
+    this.book.getContacts().then((contacts) => {
+      for (let contact of contacts) {
+        console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          ------------`
+        );
+      }
+      this.main();
+    }).catch((err) => {
+      console.log(err);
+      this.main;
     });
   }
 
